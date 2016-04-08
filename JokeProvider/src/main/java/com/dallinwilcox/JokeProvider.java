1f502686e5c26a2173fd06fefe8a8ecb71995da9
dallinwilcox <dcwilcox@gmail.com>
2016-04-07 23:40:25 -0500

    fixed errant characters and joke provider bug

--- JokeProvider/src/main/java/com/dallinwilcox/JokeProvider.java
+++ JokeProvider/src/main/java/com/dallinwilcox/JokeProvider.java
@@ -9,14 +9,14 @@ import java.util.Random;
 //referenced http://www.javapractices.com/topic/TopicAction.do?Id=62
 
 public class JokeProvider {
-    private Random rand;
+    private Random rand = new Random();
     private List<String> jokes = new ArrayList<String>(Arrays.asList(
             "Q: Why does a chicken coop have two doors?\n" +
                 "\n" + "A: If it had four, it would be a chicken sedan.",
             "Q: What do you call a parade of rabbits hopping backwards?\n" +
                 "\n" + "A: a receding hare-line.",
             "Q: What do you call an old snowman?\n" + "\n" + "A: Water!",
-            "Q: What’s the different between a cat and a comma?\n" +
+            "Q: What's the different between a cat and a comma?\n" +
                 "\n" + "A: A cat has claws at the end of paws; A comma is a pause at the end of a clause.",
             "Q: Which dinosaur knew the most words?\n" +
                 "\n" + "A: The thesaurus.",
@@ -24,7 +24,7 @@ public class JokeProvider {
                 "\n" + "A: Gets jalapeño business!"
     ));
 
-    private String getRandomJoke()
+    public String getRandomJoke()
     {
         return jokes.get(rand.nextInt(jokes.size()));
     }
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,13 +7,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.dallinwilcox.JokeProvider;
+
 
 public class MainActivity extends ActionBarActivity {
 
+    private JokeProvider jokeProvider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        jokeProvider = new JokeProvider();
     }
 
 
@@ -40,7 +44,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, jokeProvider.getRandomJoke(), Toast.LENGTH_LONG).show();
     }
 
 
